b1e5b1d4de5922fa7e57e0b06d88b38dce23a841
tomaslg <tomas.lagos.gonzalez@gmail.com>
2022-09-07 01:15:33 -0400

    Prints tables for the paper/disables plots

--- plot_visualizations.py
+++ plot_visualizations.py
@@ -65,7 +65,7 @@ class LegendObject(object):
 # os.path.join(#os.path.dirname(os.path.dirname(
 Output_dir = os.path.dirname(os.getcwd())#)),
     # "output5")
-Output_dir = [os.path.join(Output_dir,"output6"),os.path.join(Output_dir,"output7")
+Output_dir = [os.path.join(Output_dir,"output9")#,os.path.join(Output_dir,"output7")
               ]
 Name_out_file = ["path_based.pdf","path_aggregated.pdf"
                  ]
@@ -254,17 +254,16 @@ _columns = ['benchmark', 'distribution', 'theta', 'norm', 'T', 't']
     
 dataframe_all_results = {}
 _columns = ['benchmark', 'theta', 'norm', 'T', 't', 'n_sim']
-first_indicator = True
 
-for output_dir,name_out_file in zip(Output_dir,Name_out_file):
-    if first_indicator:
-        for _col in _columns:
-            dataframe_all_results[_col] = []
-        dataframe_all_results["instance"] = []
-    dataframe_all_results["regret"+"_"+name_out_file.replace(".pdf", "")] = []
-    dataframe_all_results["wins"+"_"+name_out_file.replace(".pdf", "")] = []
-    dataframe_all_results["regret_indpendent"+
-                          "_"+name_out_file.replace(".pdf", "")] = []
+for output_dir in Output_dir:
+    for _col in _columns:
+        dataframe_all_results[_col] = []
+    dataframe_all_results["instance"] = []
+    dataframe_all_results["regret"+"_PA"] = []
+    dataframe_all_results["regret"+"_PB"] = []
+    dataframe_all_results["regret_indpendent"] = []
+    dataframe_all_results["wins"+"_PA"] = []
+    dataframe_all_results["wins"+"_PB"] = []
     st_ = ""
     for (_reversed__,one_border,borders_faster,obstacle_slower,snake_opt_path
          ) in all_instances_:
@@ -287,33 +286,103 @@ for output_dir,name_out_file in zip(Output_dir,Name_out_file):
                             instance___+".csv"))
                     for key_exp,index in registered_results.groupby(
                             _columns).groups.items():
-                        if first_indicator:
-                            for ind_col,_col in enumerate(_columns):
-                                dataframe_all_results[_col
-                                    # +"_"+name_out_file.replace(".pdf", "")
-                                    ].append(key_exp[ind_col])
-                            instance_name_label_sep = instance___.split(
-                                "_")[1:]
-                            instance_name_label = ""
-                            for segment in instance_name_label_sep:
-                                instance_name_label += " " + segment
-                            dataframe_all_results["instance"].append(
-                                instance_name_label)
-                        dataframe_all_results["regret_indpendent"+
-                          "_"+name_out_file.replace(".pdf", "")].append(
+                        for ind_col,_col in enumerate(_columns):
+                            dataframe_all_results[_col
+                                # +"_"+name_out_file.replace(".pdf", "")
+                                ].append(key_exp[ind_col])
+                        instance_name_label_sep = instance___.split(
+                            "_")[1:]
+                        instance_name_label = ""
+                        for segment in instance_name_label_sep:
+                            instance_name_label += " " + segment
+                        dataframe_all_results["instance"].append(
+                            instance_name_label)
+                        dataframe_all_results["regret_indpendent"].append(
                               registered_results.loc[min(index),"regret"])
-                        ratio_regret = (
+                        #
+                        ratio_regret_PA_INdp = (
                             registered_results.loc[max(index),"regret"]/
                             registered_results.loc[min(index),"regret"])
-                        dataframe_all_results["regret"
-                            +"_"+name_out_file.replace(".pdf", "")].append(
-                                ratio_regret)
-                        dataframe_all_results["wins"
-                            +"_"+name_out_file.replace(".pdf", "")].append(
-                                1 if ratio_regret<1. else 0)
+                        ind_PB = 0
+                        for i_ in index:
+                            ind_PB = (ind_PB if i_==min(index) or i_==max(
+                                index) else i_)
+                        ratio_regret_PB_INdp = (
+                            registered_results.loc[ind_PB,"regret"]/
+                            registered_results.loc[min(index),"regret"])
+                        dataframe_all_results["regret"+"_PA"].append(
+                            ratio_regret_PA_INdp)
+                        dataframe_all_results["regret"+"_PB"].append(
+                            ratio_regret_PB_INdp)
+                        dataframe_all_results["wins"+"_PA"].append(
+                            0 if ratio_regret_PA_INdp>=1. else (
+                                0 if ratio_regret_PA_INdp>ratio_regret_PB_INdp 
+                                else 1))
+                        dataframe_all_results["wins"+"_PB"].append(
+                            0 if ratio_regret_PB_INdp>=1. else (
+                                0 if ratio_regret_PB_INdp>ratio_regret_PA_INdp 
+                                else 1))
                 except FileNotFoundError:
                     continue
-    first_indicator = False
+# first_indicator = True
+# for output_dir,name_out_file in zip(Output_dir,Name_out_file):
+#     if first_indicator:
+#         for _col in _columns:
+#             dataframe_all_results[_col] = []
+#         dataframe_all_results["instance"] = []
+#     dataframe_all_results["regret"+"_"+name_out_file.replace(".pdf", "")] = []
+#     dataframe_all_results["wins"+"_"+name_out_file.replace(".pdf", "")] = []
+#     dataframe_all_results["regret_indpendent"+
+#                           "_"+name_out_file.replace(".pdf", "")] = []
+#     st_ = ""
+#     for (_reversed__,one_border,borders_faster,obstacle_slower,snake_opt_path
+#          ) in all_instances_:
+#         # for instance___ in ["_two_border","_one_border","_with_obstacle","_snake"]:#for _reversed__ in [True,False]:
+#         # attempt_successful = False
+#         for norm_ in range(1,3):
+#             instance___ = ("_two_border" if borders_faster else (
+#                                 "_one_border" if one_border else (
+#                                 "_with_obstacle" if obstacle_slower else (
+#                                 "_chessboard" if not snake_opt_path else 
+#                                 "_snake"))) 
+#                                 )
+#             for kernel_name,kernel_thetas in Theta__.items():
+#                 try:
+#                     registered_results = pd.read_csv(
+#                         os.path.join(
+#                             output_dir,
+#                         f"SIMULATION_Regret_{kernel_name}_"+
+#                         f"norm{norm_}{'_reversed' if _reversed__ else ''}"+
+#                             instance___+".csv"))
+#                     for key_exp,index in registered_results.groupby(
+#                             _columns).groups.items():
+#                         if first_indicator:
+#                             for ind_col,_col in enumerate(_columns):
+#                                 dataframe_all_results[_col
+#                                     # +"_"+name_out_file.replace(".pdf", "")
+#                                     ].append(key_exp[ind_col])
+#                             instance_name_label_sep = instance___.split(
+#                                 "_")[1:]
+#                             instance_name_label = ""
+#                             for segment in instance_name_label_sep:
+#                                 instance_name_label += " " + segment
+#                             dataframe_all_results["instance"].append(
+#                                 instance_name_label)
+#                         dataframe_all_results["regret_indpendent"+
+#                           "_"+name_out_file.replace(".pdf", "")].append(
+#                               registered_results.loc[min(index),"regret"])
+#                         ratio_regret = (
+#                             registered_results.loc[max(index),"regret"]/
+#                             registered_results.loc[min(index),"regret"])
+#                         dataframe_all_results["regret"
+#                             +"_"+name_out_file.replace(".pdf", "")].append(
+#                                 ratio_regret)
+#                         dataframe_all_results["wins"
+#                             +"_"+name_out_file.replace(".pdf", "")].append(
+#                                 1 if ratio_regret<1. else 0)
+#                 except FileNotFoundError:
+#                     continue
+#     first_indicator = False
 
 dataframe_all_results = pd.DataFrame(dataframe_all_results)  
 _columns = ['instance','benchmark', 'theta', 'norm', 'T', 't']
@@ -329,66 +398,144 @@ avg_regret_group_indp_df = registered_results[[col_
                                     for col_ in registered_results.columns
                                     if col_!="theta" and
                                     col_!='norm' and
-                                    col_!='benchmark']].groupby([col_ 
+                                    col_!='benchmark'
+                                    ]].groupby([col_ 
                                     for col_ in _columns
                                     if col_!="theta" and 
                                     col_!='norm' and 
-                                    col_!='benchmark']).mean()
+                                    col_!='benchmark'
+                                    ]).mean()
+
+
+# st_ = ""
+# non_repeating_keys = ["","","","",""]
+# for key_exp,curve_indices in avg_regret_df.groupby([
+#         _col for _col in _columns if "t" != _col]
+#         ).groups.items():
+#     curve = avg_regret_df.loc[curve_indices]
+#     curve_err = std_regret_df.loc[curve_indices]/500**.5
+#     for key_comp_index,key_comp  in enumerate(key_exp[:2]):
+#         st_ += ("" if non_repeating_keys[key_comp_index]==key_comp else ( 
+#             r"\multirow{xx}{*}{"+(
+#             key_comp if type(key_comp) is str else (
+#             str(key_comp) if type(key_comp) is int else 
+#             str(np.round(key_comp,2)))) + "}")) + " & "
+#         non_repeating_keys[key_comp_index] = key_comp 
+#     for key_comp  in key_exp[2:-1]:
+#         st_ += (key_comp if type(key_comp) is str else (
+#             str(key_comp) if type(key_comp) is int else 
+#             str(np.round(key_comp,2)))) + " & "
+#     for t_snapshots in T_snapshots_print:
+#         # st_ += ""
+        
+#         st_ += str(np.round(curve["regret"
+#                         +"_PA"][t_snapshots-1],2))
+#         st_ += " & "
+#         st_ += str(np.round(curve["regret"
+#                         +"_PB"][t_snapshots-1],2))
+#         st_ += " & "
+#         st_ += str(np.round(avg_regret_group_indp_df.loc[
+#                 key_exp[:1]+key_exp[-1:]+(t_snapshots,)][
+#                     "regret_indpendent"],2))+" & "
+#     for t_snapshots in T_snapshots_print:
+#         # st_ += ""
+#         st_ += str(np.round(curve["wins"+"_PA"][
+#                                 t_snapshots-1],2))
+#         st_ += " & "
+#         st_ += str(np.round(curve["wins"+"_PB"][
+#                                 t_snapshots-1],2))
+#         if t_snapshots!=T_snapshots_print[-1]:
+#             st_ += " & "
+#         else:
+#             st_ += r" \\\hline"
+#     st_ += "\n"
+        
+# print(r"\multirow{3}{*}{Instance} & \multirow{3}{*}{Kernel} & "+
+#       r"\multirow{3}{*}{$\theta$} & \multirow{3}{*}{Norm} & "+
+#       r"\multicolumn{6}{|c|}{"+"Pseudo-regret} & "+
+#       r"\multicolumn{4}{|c|}{"+r"\% of wins}"+
+#       r" \\\cline{5-14}")
+# # (PB/Ind,PA/Ind)
+# print(" &  &  &  & "+
+#       r"\multicolumn{3}{|c|}{"+"$t=10$} & "+
+#       r"\multicolumn{3}{|c|}{"+"$t=50$} & "+
+#       r"\multicolumn{2}{|c|}{"+"$t=10$} & "+
+#       r"\multicolumn{2}{|c|}{"+"$t=50$} "+
+#       r" \\\cline{5-14}")
+# print(" &  &  &  & PA & PB & Ind. & PA & PB & Ind. & PA & PB & PA & PB"+
+#       r" \\\hline")
+# # print(" &  &  &  & $t=10$ & $t=20$ & $t=30$ & $t=40$ & $t=50$"+
+# #       r" \\\hline")
+
+
 st_ = ""
-
-
-
+non_repeating_keys = ["","","","",""]
 for key_exp,curve_indices in avg_regret_df.groupby([
         _col for _col in _columns if "t" != _col]
         ).groups.items():
+    first_ = True
     curve = avg_regret_df.loc[curve_indices]
-    curve_err = std_regret_df.loc[curve_indices]/100**.5
-    for key_comp  in key_exp[:-1]:
+    curve_err = std_regret_df.loc[curve_indices]/500**.5
+    print(key_exp[0])
+    for key_comp_index,key_comp  in enumerate(key_exp[1:2]):
+        st_ += ("" if non_repeating_keys[key_comp_index]==key_comp else ( 
+            r"\multirow{xx}{*}{"+("" if not first_ else (
+            key_comp if type(key_comp) is str else (
+            str(key_comp) if type(key_comp) is int else 
+            str(np.round(key_comp,2))))) + "}")) + " & "
+        non_repeating_keys[key_comp_index] = key_comp 
+    for key_comp  in key_exp[2:-1]:
         st_ += (key_comp if type(key_comp) is str else (
             str(key_comp) if type(key_comp) is int else 
             str(np.round(key_comp,2)))) + " & "
     for t_snapshots in T_snapshots_print:
         # st_ += ""
-        for name_out_file in Name_out_file:
-            st_ += str(np.round(curve["regret"
-                            +"_"+name_out_file.replace(".pdf", "")][
-                                t_snapshots-1],2))
-            st_ += " & "
-        st_ += str(np.round(
-            (avg_regret_group_indp_df.loc[
-                key_exp[:1]+key_exp[4:]+(t_snapshots,)][
-                    "regret_indpendent_path_based"]+
-             avg_regret_group_indp_df.loc[
-                key_exp[:1]+key_exp[4:]+(t_snapshots,)][
-                    "regret_indpendent_path_aggregated"])/2,2))+" & "
+        
+        st_ += str(np.round(curve["regret"
+                        +"_PA"][t_snapshots-1],2))
+        st_ += " & "
+        st_ += str(np.round(curve["regret"
+                        +"_PB"][t_snapshots-1],2))
+        st_ += " & "
+        # st_ += str(np.round(avg_regret_group_indp_df.loc[
+        #         key_exp[:1]+key_exp[-1:]+(t_snapshots,)][
+        #             "regret_indpendent"],2))+" & "
     for t_snapshots in T_snapshots_print:
         # st_ += ""
-        for name_out_file in Name_out_file:
-            st_ += str(np.round(curve["wins"
-                            +"_"+name_out_file.replace(".pdf", "")][
+        st_ += str(np.round(curve["wins"+"_PA"][
                                 t_snapshots-1],2))
-            if (name_out_file!=Name_out_file[-1] or 
-                t_snapshots!=T_snapshots_print[-1]):
-                st_ += " & "
-            else:
-                st_ += r" \\\hline"
+        st_ += " & "
+        st_ += str(np.round(curve["wins"+"_PB"][
+                                t_snapshots-1],2))
+        if t_snapshots!=T_snapshots_print[-1]:
+            st_ += " & "
+        else:
+            st_ += r" \\"
     st_ += "\n"
         
-print(r"\multirow{3}{*}{Instance} & \multirow{3}{*}{Kernel} & "+
-      r"\multirow{3}{*}{$\theta$} & \multirow{3}{*}{Norm} & "+
-      r"\multicolumn{6}{|c|}{"+"Pseudo-regret} & "+
-      r"\multicolumn{4}{|c|}{"+r"\% of wins}"+
-      r" \\\cline{5-14}")
-# (PB/Ind,PA/Ind)
-print(" &  &  &  & "+
-      r"\multicolumn{3}{|c|}{"+"$t=10$} & "+
-      r"\multicolumn{3}{|c|}{"+"$t=50$} & "+
-      r"\multicolumn{2}{|c|}{"+"$t=10$} & "+
-      r"\multicolumn{2}{|c|}{"+"$t=50$} "+
-      r" \\\cline{5-14}")
-print(" &  &  &  & PB & PA & Ind. & PB & PA & Ind. & PB & PA & PB & PA"+
-      r" \\\hline")
-# print(" &  &  &  & $t=10$ & $t=20$ & $t=30$ & $t=40$ & $t=50$"+
-#       r" \\\hline")
+
+print(r"\multirow{3}{*}{\bf Kernel} & \multirow{3}{*}{\bf $\dpar$} & \multirow{3}{*}{\bf Norm} & \multicolumn{4}{c|}{\bf Pseudo-regret} & \multicolumn{4}{c}{\bf \% of wins} \\")
+print(r"&  &  & \multicolumn{2}{c}{\bf $t=10$} & \multicolumn{2}{c|}{\bf $t=50$} & \multicolumn{2}{c}{\bf $t=10$} & \multicolumn{2}{c}{\bf $t=50$}  \\")
+print(r" &  &  & \textbf{PA} & \textbf{PB} & \textbf{PA} & \textbf{PB} & \textbf{PA} & \textbf{PB} & \textbf{PA} & \textbf{PB} \\\hline")
+# \multirow{12}{*}{exponential} & \multirow{2}{*}{0.67} & 1 & 0.82 & 0.86 & 0.68 & 0.72 & 0.48 & 0.41 & 0.5 & 0.43 \\ 
+# &  & 2 & 0.7 & 0.78 & 0.59 & 0.65 & 0.55 & 0.38 & 0.58 & 0.4 \\ \cline{2-11}
+# & \multirow{2}{*}{1.0} & 1 & 0.67 & 0.72 &  0.57 & 0.61 &  0.52 & 0.41 & 0.54 & 0.43 \\
+# &  & 2 & 0.6 & 0.69 &  0.5 & 0.57 &  0.57 & 0.4 & 0.61 & 0.38 \\ \cline{2-11}
+# & \multirow{2}{*}{1.33} & 1 & 0.58 & 0.63 & 0.49 & 0.54 &  0.52 & 0.45 & 0.54 & 0.44 \\
+# &  & 2 & 0.57 & 0.63 &  0.47 & 0.54 & 0.55 & 0.42 & 0.57 & 0.41 \\ \cline{2-11}
+# & \multirow{2}{*}{1.67} & 1 & 0.57 & 0.63 &  0.47 & 0.53 &  0.57 & 0.4 & 0.61 & 0.38 \\
+# &  & 2 & 0.55 & 0.59 & 0.45 & 0.5 & 0.54 & 0.43 & 0.6 & 0.39 \\ \cline{2-11}
+# & \multirow{2}{*}{2.0} & 1 & 0.55 & 0.59 &  0.46 & 0.52 & 0.52 & 0.46 & 0.58 & 0.41 \\
+# &  & 2 & 0.52 & 0.58 & 0.43 & 0.5 & 0.58 & 0.39 & 0.63 & 0.36 \\ \cline{2-11}
+# & \multirow{2}{*}{2.33} & 1 & 0.53 & 0.63 & 0.43 & 0.54 & 0.59 & 0.39 & 0.64 & 0.35 \\
+# &  & 2 & 0.5 & 0.54 & 0.41 & 0.48 & 0.56 & 0.42 & 0.63 & 0.36 \\ \hline 
+# \multirow{6}{*}{gaussian} & 0.67 & \multirow{6}{*}{2} & 0.9 & 0.97  & 0.77 & 0.82 & 0.43 & 0.36 & 0.49 & 0.41 \\
+# & 1.0 &  & 0.82 & 0.85 & 0.66 & 0.68 & 0.48 & 0.41 & 0.52 & 0.44 \\
+# & 1.33 &  & 0.75 & 0.74 & 0.69 & 0.62 & 0.43 & 0.48 & 0.39 & 0.58 \\
+# & 1.67 &  & 0.92 & 0.72 & 1.26 & 0.6 & 0.33 & 0.57 & 0.03 & 0.9 \\
+# & 2.0 &  & 1.24 & 0.7 & 2.01 & 0.58 & 0.13 & 0.73 & 0.0 & 0.89 \\
+# & 2.33 &  & 1.49 & 0.7 & 2.35 & 0.58 & 0.09 & 0.74 & 0.0 & 0.9 \\\hline
+
+
 print(st_)
 
