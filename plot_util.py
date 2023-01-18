108b55e131079af325288af06a1b44ded94dfcb5
tomaslg <tomas.lagos.gonzalez@gmail.com>
2023-01-17 22:23:07 -0500

    plots allow useLatex; improve plots format

--- plot_util.py
+++ plot_util.py
@@ -6,10 +6,35 @@ Created on Thu Dec  9 01:43:33 2021
 @author: tomas
 """
 
-from TS import nx
+from TS import nx,format_distrib_name
 from distrib import stats,np
 
+
+# import matplotlib.pyplot as plt
+import pandas as pd
+import subprocess
+# subprocess.check_output('conda activate geo', shell=True)
+import geopandas as gpd
+# import psycopg2
+from shapely.geometry import LineString
+from matplotlib.font_manager import FontProperties
+
 show_plot = False
+
+font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+font = FontProperties(size=16)
+font.set_family('serif')
+font_dict = {'fontsize': 'x-large', 'fontproperties': font}
+
+def replace_infty_by_max(np, ar):
+    max_no_infty = max( [a for a in ar if np.abs(a)!=np.inf] )
+    min_no_infty = min( [a for a in ar if np.abs(a)!=np.inf] )
+    return np.array( [a  if np.abs(a)!=np.inf else (
+        max_no_infty if np.sign(a)==1 else min_no_infty) for a in ar])
 
 def plot_distribution(distrib_,pp,plt_,Title=""):
     # Yx=distrib_.sample_poserior()
@@ -20,13 +45,13 @@ def plot_distribution(distrib_,pp,plt_,Title=""):
     fig, ax_ = plt_.subplots(1, 1, sharey=True, tight_layout=True)
     ax_.plot(_x_, dist_mu_.pdf(_x_),'r-', lw=5, alpha=0.6, label='mu pdf')
     ax_.set_title(f"{Title}_{distrib_.name}")
-    font = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 16,
-        }
-    plt_.xlabel('mu', fontdict=font)
-    plt_.ylabel('density', fontdict=font)
+    # font = {'family': 'serif',
+    #     'color':  'darkred',
+    #     'weight': 'normal',
+    #     'size': 16,
+    #     }
+    plt_.xlabel('mu', fontdict=font_dict)
+    plt_.ylabel('density', fontdict=font_dict)
     if pp==False:
         plt_.show()
     else:
@@ -39,13 +64,13 @@ def plot_histogram_sigma(distrib_,pp,plt_,Title=""):
     fig, ax_ = plt_.subplots(1, 1, sharey=True, tight_layout=True)
     ax_.hist(Yx, bins=1000)
     ax_.set_title(f"{Title}_{distrib_.name}")
-    font = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 16,
-        }
-    plt_.xlabel('lambda', fontdict=font)
-    plt_.ylabel('frec.', fontdict=font)
+    # font = {'family': 'serif',
+    #     'color':  'darkred',
+    #     'weight': 'normal',
+    #     'size': 16,
+    #     }
+    plt_.xlabel('lambda', fontdict=font_dict)
+    plt_.ylabel('frec.', fontdict=font_dict)
     if pp==False:
         plt_.show()
     else:
@@ -62,12 +87,17 @@ def get_plot(G,edge_color_,pp,plt_,Title="",_paths_=[]):
     # nx.draw(G)  # networkx draw()
     # plt.draw()  # pyplot draw()
     # pp = PdfPages(f'graph_speeds_N{N}_M{M}.pdf')
+    plt_.rcParams['text.usetex'] = True
     ax = plt_.gca()
-    ax.set_title(Title)
+    ax.set_title(Title, fontdict=font_dict)
+    if all([type(ec) is float or isinstance(ec, np.floating)
+            for ec in edge_color_]):
+        edge_color_ = replace_infty_by_max(np, edge_color_)
     nx.draw_networkx(G, with_labels = False,pos=pos,
-                     node_size=100/(len(G.nodes())**.5),
+                     node_size=1/(len(G.nodes())**.5),
                      arrowsize=60/(len(G.nodes())**.5),
-                     edge_cmap=plt_.get_cmap("RdYlGn_r"),#edge_cmap=plt_.get_cmap("RdYlGn"),#plt.cm.Blues,cmap="RdYlGn",
+                     width=60/(len(G.nodes())**.5),
+                     edge_cmap=plt_.get_cmap("RdYlGn"),#edge_cmap=plt_.get_cmap("RdYlGn"),#plt.cm.Blues,cmap="RdYlGn",
                      edge_color=edge_color_,
                      # missing_kwds= dict(color = "k",)
                      )
@@ -80,7 +110,7 @@ def get_plot(G,edge_color_,pp,plt_,Title="",_paths_=[]):
         #     if path_pos[len_path_-i-1] in path_pos[:(len_path_-i-1)]:
         #         path_pos.pop(len_path_-i-1)
         nx.draw_networkx_nodes(G,pos,nodelist=list(path_pos.keys()),
-                               node_size=100/(len(G.nodes())**.5),node_color='k')
+                               node_size=1/(len(G.nodes())**.5),node_color='k')
         nx.draw_networkx_edges(G,pos,edgelist=_paths_,edge_color='k',
                                width=60/(len(G.nodes())**.5))
         # nx.draw_networkx_edges(
@@ -98,29 +128,30 @@ def get_plot(G,edge_color_,pp,plt_,Title="",_paths_=[]):
 
     plt_.close()
 
+
 def plot_regret_t(prior_Spatial_name,regret_NormalIG,regret_Spatial_,pp,plt_):
     if pp==False and not show_plot: return None
     plt_.plot((list(range(1,len(regret_NormalIG)+1))), 
              regret_NormalIG, 
-             label='sum(regret_NormalIG)/T='+
+             label="NormalIG = " +#'sum(regret_NormalIG)/T='+
              f'{np.round(regret_NormalIG[-1],2)}', 
              color='k')
-    colors_ = ['b','r']
+    colors_ = ['b','r', "g", "m", "c"]
     for _color, regret_Spatial, name in zip(colors_, regret_Spatial_, prior_Spatial_name): 
         plt_.plot((list(range(1,len(regret_Spatial)+1))), 
                  regret_Spatial, 
-                 label='sum(regret_'+
-                 f'{name})/T={np.round(regret_Spatial[-1],2)}', 
+                 label= format_distrib_name(name) + # 'sum(regret_'+
+                  f' = {np.round(regret_Spatial[-1],2)}', 
                  color=_color)
     plt_.legend()
-    font = {'family': 'serif',
-            'color':  'darkred',
-            'weight': 'normal',
-            'size': 16,
-            }
-    plt_.title('Travel Time Regret', fontdict=font)
-    plt_.xlabel('periods', fontdict=font)
-    plt_.ylabel('sum(regret_t)/t [hrs]', fontdict=font)
+    # font = {'family': 'serif',
+    #         'color':  'darkred',
+    #         'weight': 'normal',
+    #         'size': 16,
+    #         }
+    plt_.title('Travel Time Regret', fontdict=font_dict)
+    plt_.xlabel('periods', fontdict=font_dict)
+    plt_.ylabel('[hrs]', fontdict=font_dict)
     
     # Tweak spacing to prevent clipping of ylabel
     plt_.subplots_adjust(left=0.15)
@@ -144,17 +175,9 @@ def get_graph_instance_2(N=20):
 
 
 
-# import matplotlib.pyplot as plt
-import pandas as pd
-import subprocess
-# subprocess.check_output('conda activate geo', shell=True)
-import geopandas as gpd
-# import psycopg2
-from shapely.geometry import LineString
-from matplotlib.font_manager import FontProperties
 
     
-def plot_real_life_instance(values,title,pp,plt_):
+def plot_real_life_instance(values,title,pp,plt_,color_scale_='RdYlGn'):
     # data 
     # sql = """select w.id as arc, n1.latitude as lat1, n1.longitude as lon1, 
     #      n2.latitude as lat2, n2.longitude as lon2, s.speed as speed
@@ -169,19 +192,26 @@ def plot_real_life_instance(values,title,pp,plt_):
                                                     float(values[i][1])], 
                                                   [float(values[i][4]), 
                                                     float(values[i][3])]]))
-    data['speed'] = [float(values[i][5]) for i in range(len(values))]
+    data['speed'] = [(1 if i==0 else (-1 if i==1 else 0
+        )) if not np.isnan(float(values[i][
+        5])) and color_scale_=="gist_yarg" else float(
+        values[i][5]) for i in range(len(values))]
+    print(title)
+    print(f"min()={min(data['speed'])}",
+          f"max()={max(data['speed'])}",
+          f"mean()={np.mean(data['speed'])}")
     df = pd.DataFrame(data)
     geo_df = gpd.GeoDataFrame(df, geometry = 'LineString_obj')
     
     # create figure and axes, assign to subplot
-    fig, ax = plt_.subplots(figsize=(15,15)) 
-    geo_df.plot(column='speed', ax=ax, legend=True, cmap='RdYlGn', 
-                legend_kwds={'shrink': 0.45, 'label': 'meters/second'},
-                missing_kwds= dict(color = "k",)) #lightgrey brg
-    font = FontProperties()
-    #font.set_family('serif')
+    fig, ax = plt_.subplots(figsize=(7,6))
+    geo_df.plot(column='speed', ax=ax, legend=color_scale_!="gist_yarg",
+                cmap=color_scale_, legend_kwds={'shrink': 0.45, 'label': 'meters/second'},
+                missing_kwds= dict(color = "k", linewidth=2)) #lightgrey brg
+    # font = FontProperties()
+    # font.set_family('serif')
     plt_.title(title#"Beijing"
-                , fontdict={'fontsize': 'x-large', 'fontproperties': font}, fontweight='bold')
+                , fontdict={'fontsize': 'large', 'fontproperties': font})
     plt_.ylabel("Latitude", fontdict={'fontsize': 'large', 'fontproperties': font})
     plt_.xlabel("Longitude", fontdict={'fontsize': 'large', 'fontproperties': font})
     # fig.savefig("../../real_instance.pdf", bbox_inches='tight')
