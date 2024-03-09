58885e28875559628f7dd13f2d6b9340a19fc902
tomaslg <tomas.lagos.gonzalez@gmail.com>
2024-03-09 00:55:21 -0300

    plots regret PA&PB to display scope-params&norms

--- plot_avg_regret_artificial_instance.py
+++ plot_avg_regret_artificial_instance.py
@@ -26,8 +26,9 @@ def plot_regret_t(prior_Spatial_name,regret_NormalIG,regret_Spatial_,title_,pp,p
     style_ = [":", "-.", "--", "-"]
     avg_start_O_1_t = max_start_O_1_t = regret_NormalIG[0]
     cnt_ = 1
-    for _style,_color, regret_Spatial, name in zip(
-            style_, colors_, regret_Spatial_, prior_Spatial_name):
+    for _style,_color, regret_Spatial, name, _title_ in zip(
+            style_, colors_, regret_Spatial_, prior_Spatial_name,
+            title_):
         avg_start_O_1_t = avg_start_O_1_t * cnt_ + regret_Spatial[0]
         cnt_ +=1
         avg_start_O_1_t /= cnt_
@@ -35,10 +36,10 @@ def plot_regret_t(prior_Spatial_name,regret_NormalIG,regret_Spatial_,title_,pp,p
         plt_.plot((list(range(1,len(regret_Spatial)+1))), 
                  regret_Spatial, 
                  linestyle=_style,
-                 label= name + f" ({title_})",#' = {np.round(regret_Spatial[1][-1],2)}', 
+                 label= name + f" ({_title_})",#' = {np.round(regret_Spatial[1][-1],2)}', 
                  color=_color)
     plt_.plot((list(range(1,len(regret_NormalIG)+1))), 
-            [max_start_O_1_t / ((t + 1) ** .5) for t in range(
+            [avg_start_O_1_t / ((t + 1) ** .5) for t in range(
                 len(regret_NormalIG))], 
             label= r"$O(1/\sqrt{t})$",
             color=colors_[-1])
@@ -64,64 +65,101 @@ def plot_regret_t(prior_Spatial_name,regret_NormalIG,regret_Spatial_,title_,pp,p
 
 # output_dir = r"C:\Users\tol28\Dropbox\OTDSPP\codes\output9"
 output_dir = os.path.join(os.path.dirname(
-        os.getcwd()), "output_beta3_alpha1")
+        os.getcwd()), "output_beta3_alpha1_20_150")
 
-T_last_iteration = 50
+T_last_iteration = 150
 
 pp = PdfPages(os.path.join(output_dir,
         f"regret_artificial_indp_vs_spatial.pdf"))
 
-
+kernel_type = "exponential"
 files = [os.path.join(output_dir, fl_) for fl_ in os.listdir(output_dir)
-    if fl_.split(".")[-1]== "csv" and "norm1" in fl_ and "exponential" in fl_]
+    if fl_.split(".")[-1]== "csv"# and "norm1" in fl_ 
+    and kernel_type in fl_]
 
 
 theta_parameters = [#8/3, 10/3, 2/3, 8/3
     #4., 4., 2/3, 8/3 #
-                    # 4., 4., 
-                    2., 4.
+                    (4/3., 8/3.), (2., 4.), 
+                    (2., 8/3.), (2., 4.)
     # [4.,4.],[4.,4.],[4/3,4/3],[4.,4.]
     ]
 expert_objective = [0.7694762998838899,#one_border 1
                     202.49349548634692,#snake 3
                     1.4431074645596709,#two_border 4
                     0.12495338590266658]#with_obstacle 2
+norm_spatial = [(2, 1), (1, 2), (2, 2), (2, 2)]
 # ['benchmark', 'distribution', 'theta', 'norm', 'n_sim', 'T', 't', 'regret']
 _columns = ['benchmark', 'distribution', 'theta', 'norm', 'T', 't']
-
+instances_names = ["one_border",
+                   "snake",
+                   "two_border",
+                   "with_obstacle"]
 for k,thetas in enumerate(theta_parameters):
-    regret = pd.read_csv(files[k])
-    kernel_type = files[k].split("\\")[-1].split(".")[0].split("_")
-    kernel_type,norm_str,instance = kernel_type[2], kernel_type[3], kernel_type[4:]
+    file_PB = [fl_ for fl_ in files
+                      if instances_names[k] in fl_
+                      and f"norm{norm_spatial[k][0]}" in fl_][0]
+    regret_data_PB = pd.read_csv(file_PB)
+    file_PA = [fl_ for fl_ in files
+                      if instances_names[k] in fl_
+                      and f"norm{norm_spatial[k][1]}" in fl_][0]
+    regret_data_PA = pd.read_csv(file_PA)
+    # regret = pd.read_csv(files[k])
+    # kernel_type = files[k].split("\\")[-1].split(".")[0].split("_")
+    # kernel_type,norm_str,instance = kernel_type[2], kernel_type[3], kernel_type[4:]
     
-    data_for_plots = regret[(regret["benchmark"]==kernel_type) & (
-        regret["theta"].between(thetas - .1**6, thetas + .1**6)#[0]
+    # data_for_plots = regret[(regret["benchmark"]==kernel_type) & (
+    #     regret["theta"].between(thetas - .1**6, thetas + .1**6)#[0]
+    #     )]
+    data_for_plots_PB = regret_data_PB[(
+        regret_data_PB["benchmark"]==kernel_type) & (
+        regret_data_PB["theta"].between(thetas[0] - .1**6, thetas[0] + .1**6)#[0]
+        )]
+    data_for_plots_PA = regret_data_PA[(
+        regret_data_PA["benchmark"]==kernel_type) & (
+        regret_data_PA["theta"].between(thetas[1] - .1**6, thetas[1] + .1**6)#[0]
         )]
     
-    avg_regret_df = data_for_plots.groupby(_columns).mean()
+    # avg_regret_df = data_for_plots.groupby(_columns).mean()
+    avg_regret_df_PB = data_for_plots_PB.groupby(_columns).mean()
+    avg_regret_df_PA = data_for_plots_PA.groupby(_columns).mean()
     
     Indpendent = {}
     PB = {}
     PA = {}
+    # for (kernel_, distrib_, theta_, norm_, last_iter, _t),(
+    #         n_sim_,regret_) in  avg_regret_df.iterrows():
+    #     if "IGM" in distrib_:
+    #         Indpendent[_t] = regret_ / expert_objective[k]
+    #     elif "PA" in distrib_:
+    #         PA[_t] = regret_ / expert_objective[k]
+    #     elif "PB" in distrib_:
+    #         PB[_t] = regret_ / expert_objective[k]
     for (kernel_, distrib_, theta_, norm_, last_iter, _t),(
-            n_sim_,regret_) in  avg_regret_df.iterrows():
+            n_sim_,regret_) in  avg_regret_df_PB.iterrows():
         if "IGM" in distrib_:
-            Indpendent[_t] = regret_ / expert_objective[k]
-        elif "PA" in distrib_:
-            PA[_t] = regret_ / expert_objective[k]
+            Indpendent[_t] = regret_ / expert_objective[k] / 2
         elif "PB" in distrib_:
             PB[_t] = regret_ / expert_objective[k]
+    for (kernel_, distrib_, theta_, norm_, last_iter, _t),(
+            n_sim_,regret_) in  avg_regret_df_PA.iterrows():
+        if "IGM" in distrib_:
+            Indpendent[_t] += regret_ / expert_objective[k] / 2
+        elif "PA" in distrib_:
+            PA[_t] = regret_ / expert_objective[k]
     
-    title_ = (
+    title_ = [
         # ("Case 1"if instance[0]=="one" else (
         # "Case 2" if instance[0]=="with" else (
         #     "Case 3" if instance[0]=="snake" else (
         #         "Case 4" if instance[0]=="two" else "")))
         # ) +
         # "Kernel {kernel_type} {norm_str} "
-        f"$\\varphi = {np.round(thetas,2)}$"
+        (f"$\\varphi = {np.round(thetas[i_],2)}$" + r" norm $=\|\cdot\|_{" +
+        f"{norm_spatial[k][i_]}" + "}$")
+        for i_ in [1, 0]
         # f"$\\varphi = {np.round(thetas[0],2)}$"
-        )
+        ]
     
     plot_regret_t(["PA", "PB"], [Indpendent[_t] for _t in range(
         1, T_last_iteration + 1)],
